378e3f6e10f792f751090297796fecb209cbed0b
Ritu-bisen <ritu2bisen@gmail.com>
2024-08-06 06:48:05 +0530

    on click you can seen the impact of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 
 function App() {
   return (
    <BrowserRouter>
+   <header>
+    <nav>
+      <h1>Find JObs ?</h1>
+      <Link to='/'>Home</Link>
+      <NavLink to='about'>About</NavLink>
+    </nav>
+   </header>
    <main>
     <Routes>
       <Route path="/" element={<Home/>} />
